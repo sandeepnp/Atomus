980004b19ef6e55006ad010d1c07b762b37349f9
Sandeep Narayan <sandeepnp@hotmail.com>
2017-11-27 20:23:27 +0000

    Add files via upload

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AtomusFileUpload.Models
+﻿
+namespace Atomus.Models
 {
     public class Employee
     {
